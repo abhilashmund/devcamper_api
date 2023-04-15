0f01c6ed9ac8f416eafdd4bdbcb79f93ccf98a91
Abhilash Mund <14253322+abhilashmund@users.noreply.github.com>
2023-04-15 15:34:18 +0530

    Controllers, routers , mongoose etc

---
+++ routes/bootcamps.js
@@ -0,0 +1,23 @@
+/**
+ * This has all the routes for bootcamps
+ */
+const express = require('express');
+const {
+    getBootcamps,
+    getBootcamp,
+    createBootcamp,
+    updateBootcamp,
+    deleteBootcamp,
+} = require('../controllers/bootcamps');
+
+const router = express.Router();
+
+router.route('/').get(getBootcamps).post(createBootcamp);
+
+router
+    .route('/:id')
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
+
+module.exports = router;
