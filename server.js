cd3eae0d42d42f71c11b2a596c053116e4e6fecf
Abhilash Mund <14253322+abhilashmund@users.noreply.github.com>
2023-04-15 15:45:36 +0530

    Added colors

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-const connectDB = require('./config/db')
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Route files
 const bootcamps = require('./routes/bootcamps');
@@ -26,12 +27,12 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
     PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`);
+    console.log(`Error: ${err.message}`.red.italic.underline.bold);
     //Close server & exit
     server.close(() => process.exit(1));
 })
